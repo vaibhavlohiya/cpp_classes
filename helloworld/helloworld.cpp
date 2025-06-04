862c2eedeb5d9bf2307b0a4fde0fe9bc8bce7866
vaibhavlohiya <vaibhavlohiya05@gmail.com>
2025-06-04 13:11:48 -0500

    Hello World and basic operations.

---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,13 @@
+// This is a simple C++ program that prints "Hello, World!" to the console.
+
+#include <iostream>
+
+void log(const char* message);
+
+int main()
+{
+    log("Hello, World!");
+    std::cin.get(); // Wait for user input before closing the console
+}
+
+
---
+++ operations/operations.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    int number_1, number_2;
+    std::cout << "Enter two numbers" << std::endl;
+    std::cout << "First number: ";
+    std::cin >> number_1;
+    std::cout << "Second number: ";
+    std::cin >> number_2;
+
+    std::cout << "The result is " << number_1 + number_2 << std::endl;
+    return 0;
+}
